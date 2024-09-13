7673a599fb4ef2f3a794d6e5e001863558c48e51
newoo <newoo4297@naver.com>
2024-09-13 22:55:55 +0900

    Initial Commit

---
+++ madmax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  madmax
+//
+//  Created by Taeheon Woo on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ madmax/madmaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  madmaxApp.swift
+//  madmax
+//
+//  Created by Taeheon Woo on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct madmaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
