0fe3a4cfc44e57f80ae35164d5b6e0062ebc7656
Applebuddy <applebuddy@nate.com>
2024-09-27 15:54:03 +0900

    feat. 비트맥스 웹소켓 기능 포함한 프로젝트 파일 추가

---
+++ madmax/Domain/Entity/BitmaxTradeEntity.swift
@@ -0,0 +1,14 @@
+//
+//  BitmaxTradeEntity.swift
+//  BitmaxTrading
+//
+//  Created by Min Min on 9/19/24.
+//
+
+import Foundation
+
+struct TradeItemEntity {
+  let id: Int
+  let price: Double
+  let side: String
+}
